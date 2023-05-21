60ca18e246bb0cae0de85e7bcd3ea61c05da2aad
Anna <ann111.ruu@gmail.com>
2023-05-22 01:58:39 +0300

    peace

--- client.c
+++ client.c
@@ -10,12 +10,14 @@
 #define SERVER_IP "127.0.0.1"
 #define PORT 12345
 
-void *read_messages(void *arg) {
+void *read_messages(void *arg) 
+{
     int client_socket = *(int*)arg;
     char buffer[256];
     int bytes_read;
 
-    while ((bytes_read = read(client_socket, buffer, sizeof(buffer))) > 0) {
+    while ((bytes_read = read(client_socket, buffer, sizeof(buffer))) > 0) 
+    {
         buffer[bytes_read] = '\0';
         printf("%s", buffer);
     }
@@ -29,7 +31,8 @@ int main() {
     char name[256];
     char buffer[256];
 
-    if ((client_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+    if ((client_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
+    {
         perror("Ошибка создания сокета клиента");
         exit(EXIT_FAILURE);
     }
@@ -39,7 +42,8 @@ int main() {
     server_address.sin_addr.s_addr = inet_addr(SERVER_IP);
     server_address.sin_port = htons(PORT);
 
-    if (connect(client_socket, (struct sockaddr *) &server_address, sizeof(server_address)) == -1) {
+    if (connect(client_socket, (struct sockaddr *) &server_address, sizeof(server_address)) == -1) 
+    {
         perror("Ошибка подключения к серверу");
         exit(EXIT_FAILURE);
     }
@@ -51,14 +55,16 @@ int main() {
     write(client_socket, name, strlen(name));
 
     pthread_t thread_id;
-    if (pthread_create(&thread_id, NULL, read_messages, &client_socket) != 0) {
+    if (pthread_create(&thread_id, NULL, read_messages, &client_socket) != 0) 
+    {
         perror("Ошибка создания потока чтения сообщений");
         exit(EXIT_FAILURE);
     }
 
     while (fgets(buffer, sizeof(buffer), stdin)) 
     {
-        if (strcmp(buffer, "exit\n") == 0) {
+        if (strcmp(buffer, "exit\n") == 0) 
+        {
             break;
         }
         write(client_socket, buffer, strlen(buffer));
--- server.c
+++ server.c
@@ -8,9 +8,10 @@
 #include <arpa/inet.h>
 
 #define PORT 12345
-#define SERVER_IP "127.0.0.1" // IP адрес сервера
+#define SERVER_IP "127.0.0.1"
 
-struct client {
+struct client 
+{
     int socket;
     char name[256];
 };
@@ -19,60 +20,76 @@ struct client *clients = NULL;
 int num_clients = 0;
 
 
-void list_members(int client_socket) {
+void list_members(int client_socket) 
+{
     char message[1024];
     int i;
 
-    snprintf(message, sizeof(message), "Список активных участников:\n");
+    snprintf(message, sizeof(message), "Список активных участников чата:\n");
     write(client_socket, message, strlen(message));
 
-    for (i = 0; i < num_clients; i++) {
+    for (i = 0; i < num_clients; i++) 
+    {
         snprintf(message, sizeof(message), "%s\n", clients[i].name);
         write(client_socket, message, strlen(message));
     }
 }
 
-void *handle_client(void *arg) {
+void *handle_client(void *arg) 
+{
     struct client *client_data = (struct client*)arg;
     int client_socket = client_data->socket;
     char name[256];
     char buffer[256];
     char message[1024];
-    int bytes_read;
+    int bytes;
     int i;
 
-    bytes_read = read(client_socket, name, sizeof(name));
-    name[bytes_read] = '\0';
+    bytes = read(client_socket, name, sizeof(name));
+    name[bytes] = '\0';
     strcpy(client_data->name, name);
 
-    snprintf(message, sizeof(message), "Добро пожаловать, %s! Это самый безопасный чат, все сообщения зашифрованы:\n", name);
+    snprintf(message, sizeof(message), "Добро пожаловать в самый безопасный чат, %s!\n", name);
     write(client_socket, message, strlen(message));
 
-    snprintf(message, sizeof(message), "Клиент %s подключился к чату\n", name);
-    for (i = 0; i < num_clients; i++) {
-        if (clients[i].socket != client_socket) {
+    snprintf(message, sizeof(message), "Клиент %s зашёл в чат\n", name);
+    for (i = 0; i < num_clients; i++) 
+    {
+        if (clients[i].socket != client_socket) 
+        {
             write(clients[i].socket, message, strlen(message));
         }
     }
 
     num_clients++;
-    clients = (struct client*)realloc(clients, num_clients * sizeof(struct client));
+    struct client *new_clients = (struct client*)realloc(clients, num_clients * sizeof(struct client));
+    if (new_clients == NULL) {} 
+    else 
+    {
+        clients = new_clients;
+    }    
     clients[num_clients-1].socket = client_socket;
     strcpy(clients[num_clients-1].name, name);
 
-while ((bytes_read = read(client_socket, buffer, sizeof(buffer))) > 0) {
-    if (buffer[bytes_read-1] == '\n') {
-        buffer[bytes_read-1] = '\0';
-        bytes_read--;
+while ((bytes = read(client_socket, buffer, sizeof(buffer))) > 0) 
+{
+    if (buffer[bytes-1] == '\n') 
+    {
+        buffer[bytes-1] = '\0';
+        bytes--;
     }
 
-    if (strcmp(buffer, "/members") == 0) {
+    if (strcmp(buffer, "/members") == 0) 
+    {
         list_members(client_socket);
-    } else {
+    } else 
+    {
         snprintf(message, sizeof(message), "%s: %s\n", name, buffer);
 
-        for (i = 0; i < num_clients; i++) {
-            if (clients[i].socket != client_socket) {
+        for (i = 0; i < num_clients; i++) 
+        {
+            if (clients[i].socket != client_socket) 
+            {
                 write(clients[i].socket, message, strlen(message));
             }
         }
@@ -81,13 +98,27 @@ while ((bytes_read = read(client_socket, buffer, sizeof(buffer))) > 0) {
     }
 }
 
-    for (i = 0; i < num_clients; i++) {
-        if (clients[i].socket == client_socket) {
+    snprintf(message, sizeof(message), "Клиент %s вышел из чата\n", name);
+
+    for (i = 0; i < num_clients; i++) 
+    {
+        if (clients[i].socket != client_socket) 
+        {
+            write(clients[i].socket, message, strlen(message));
+        }
+    }
+
+    for (i = 0; i < num_clients; i++) 
+    {
+        if (clients[i].socket == client_socket) 
+        {
             clients[i].socket = -1;
             break;
         }
     }
-    for (i = i+1; i < num_clients; i++) {
+
+    for (i = i+1; i < num_clients; i++) 
+    {
         clients[i-1] = clients[i];
     }
     num_clients--;
@@ -98,40 +129,46 @@ while ((bytes_read = read(client_socket, buffer, sizeof(buffer))) > 0) {
     return NULL;
 }
 
-int main() {
+int main() 
+{
     int server_socket;
-    struct sockaddr_in server_address;
+    struct sockaddr_in address;
     struct sockaddr_in client_address; 
     pthread_t threads;
     socklen_t client_address_len = sizeof(client_address);
 
     clients = (struct client*)malloc(0);
 
-    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
+    {
         perror("Ошибка создания сокета сервера");
         exit(EXIT_FAILURE);
     }
 
-    memset(&server_address, 0, sizeof(server_address));
-    server_address.sin_family = AF_INET;
-    server_address.sin_addr.s_addr = inet_addr(SERVER_IP);;
-    server_address.sin_port = htons(PORT);
+    memset(&address, 0, sizeof(address));
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = inet_addr(SERVER_IP);;
+    address.sin_port = htons(PORT);
 
-    if (bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address)) == -1) {
+    if (bind(server_socket, (struct sockaddr*)&address, sizeof(address)) == -1) 
+    {
         perror("Ошибка привязки сокета к адресу сервера");
         exit(EXIT_FAILURE);
     }
 
-    if (listen(server_socket, 10) == -1) {
+    if (listen(server_socket, 10) == -1) 
+    {
         perror("Ошибка начала прослушивания сокета");
         exit(EXIT_FAILURE);
     }
 
-    printf("ФСБ теперь прослушивает сервер %s и порт %d\n", SERVER_IP, PORT);
+    printf("ФСБ теперь прослушивает сервер %s, порт %d\n", SERVER_IP, PORT);
 
-    while (1) {
+    while (1) 
+    {
         int client_socket = accept(server_socket, (struct sockaddr*)&client_address, &client_address_len);
-        if (client_socket == -1) {
+        if (client_socket == -1) 
+        {
             perror("Ошибка принятия нового подключения");
             continue;
         }
